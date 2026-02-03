75971f8e93cb9eb68d25280af6f214744eeedac0
slvcn-jsh <salvacion.jsh@gmail.com>
2026-02-04 04:16:29 +0800

    Bump app version for offline/validation update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.pcosina.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.1.3"
+        versionCode = 15
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         

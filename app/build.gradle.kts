d620b39945e9f9771d6cadfba5823cd314bd62cb
slvcn-jsh <salvacion.jsh@gmail.com>
2026-02-04 13:40:46 +0800

    release: 1.06.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.pcosina.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.06"
+        versionCode = 9
+        versionName = "1.06.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         

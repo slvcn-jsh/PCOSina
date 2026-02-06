67705f7c88d7b70fe2db2a76fb40c9f3c25599fe
slvcn-jsh <salvacion.jsh@gmail.com>
2026-02-06 14:30:25 +0800

    Bump version to 1.08.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.pcosina.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.08.1"
+        versionCode = 18
+        versionName = "1.08.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         

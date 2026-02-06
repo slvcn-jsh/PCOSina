d9b6b7234d1f4281ac7683265e01728ee35eaae4
slvcn-jsh <salvacion.jsh@gmail.com>
2026-02-06 20:59:22 +0800

    Bump version to 1.08.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.pcosina.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 21
-        versionName = "1.08.5"
+        versionCode = 22
+        versionName = "1.08.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         

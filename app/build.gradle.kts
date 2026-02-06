68b398be97be3572a67ae176e97d4ede8845fe1c
slvcn-jsh <salvacion.jsh@gmail.com>
2026-02-07 04:07:39 +0800

    Bump versionCode to 29

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.pcosina.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 28
+        versionCode = 29
         versionName = "1.08.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

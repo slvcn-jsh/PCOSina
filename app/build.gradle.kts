3aa9b45f5681a2a1de305b8180633ee9374315f4
slvcn-jsh <salvacion.jsh@gmail.com>
2026-02-07 18:23:38 +0800

    chore: bump version to 1.10.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.pcosina.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 35
-        versionName = "1.09.3"
+        versionCode = 36
+        versionName = "1.10.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         

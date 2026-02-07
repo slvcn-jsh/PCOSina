b82f871bd59965c3e11625a65e851754b921d449
slvcn-jsh <salvacion.jsh@gmail.com>
2026-02-07 17:58:35 +0800

    chore: bump version to 1.09.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.pcosina.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 33
-        versionName = "1.09.1"
+        versionCode = 34
+        versionName = "1.09.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
